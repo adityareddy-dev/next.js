08396fd0ef6cf5907c9dd590fe83cf349cd0f47e
Will Binns-Smith <wbinnssmith@gmail.com>
2026-09-16 17:56:49 -0700

    Test bundle analyzer route summaries

--- apps/bundle-analyzer/components/analyzer.tsx
+++ apps/bundle-analyzer/components/analyzer.tsx
@@ -493,12 +493,14 @@ function ValidComparisonContent({
     [baselineRouteSummaries]
   )
   const routeDiff = useMemo(() => {
-    return diffRoutesWithSizes(
-      baselineRoutes,
-      model.currentRoutes,
-      baselineRouteTotals,
-      model.currentRouteTotals
-    )
+    return baselineRouteTotals && model.currentRouteTotals
+      ? diffRoutesWithSizes(
+          baselineRoutes,
+          model.currentRoutes,
+          baselineRouteTotals,
+          model.currentRouteTotals
+        )
+      : null
   }, [
     baselineRoutes,
     model.currentRoutes,
